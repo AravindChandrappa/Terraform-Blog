c963f27f77708c9572156800d3b42a0f1424e45b
AravindChandrappa <46191186+AravindChandrappa@users.noreply.github.com>
2023-05-14 21:34:03 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,7 +1,21 @@
 provider "aws" {
   region     = "us-east-2"
-  
-  
+  access_key = "AKIAXOLJCO4BWCHJ4EO5"
+  secret_key = "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" 
+}
+resource "tls_private_key" "pk" {
+  algorithm = "RSA"
+  rsa_bits  = 4096
+}
+
+resource "aws_key_pair" "kp" {
+  key_name   = "myKey"       # Create a "myKey" to AWS!!
+  public_key = tls_private_key.pk.public_key_openssh
+}
+
+resource "local_file" "ssh_key" {
+  filename = "${aws_key_pair.kp.key_name}.pem"
+  content = tls_private_key.pk.private_key_pem
 }
 
 #Create a new EC2 launch configuration
