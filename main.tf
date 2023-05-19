b73246bdbaa5f08d969206b875d4c2fb35b3a36f
AravindChandrappa <46191186+AravindChandrappa@users.noreply.github.com>
2023-05-19 15:55:57 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -33,7 +33,7 @@ resource "aws_instance" "ec2_public" {
     user = "ubuntu"
     type = "ssh"
     host = self.public_ip
-    private_key = aws_key_pair.kp.key_name
+    key_file = aws_key_pair.kp.key_name
     # The connection will use the local SSH agent for authentication.
   }
 
