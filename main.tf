768cffed186e948a8508e3e0776d0d2afc4d490c
AravindChandrappa <46191186+AravindChandrappa@users.noreply.github.com>
2023-05-19 11:22:37 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -30,10 +30,8 @@ resource "aws_instance" "ec2_public" {
   #iam_instance_profile = "${aws_iam_instance_profile.some_profile.id}"
   user_data = <<-EOF
 	    #!/bin/bash
-	    sudo sh -c 'echo "deb http://apt.postgresql.org/pub/repos/apt $(lsb_release -cs)-pgdg main" > /etc/apt/sources.list.d/pgdg.list'
-	    wget --quiet -O - https://www.postgresql.org/media/keys/ACCC4CF8.asc | sudo apt-key add -
 	    sudo apt-get update
-	    sudo apt-get -y install postgresql
+	    sudo apt install postgresql postgresql-contrib
    EOF
   lifecycle {
     create_before_destroy = true
