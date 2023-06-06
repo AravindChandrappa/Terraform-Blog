29e271d4c98109315bd943a386692ebefa354db6
AravindChandrappa <46191186+AravindChandrappa@users.noreply.github.com>
2023-06-06 16:37:41 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -9,7 +9,7 @@ resource "tls_private_key" "pk" {
 }
 
 resource "aws_key_pair" "kp" {
- key_name   = "myKey2"       # Create a "myKey" to AWS!!
+ key_name   = "myKey3"       # Create a "myKey" to AWS!!
   public_key = tls_private_key.pk.public_key_openssh
 } 
 
